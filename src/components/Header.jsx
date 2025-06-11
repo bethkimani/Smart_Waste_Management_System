1a403896191ddf9cfc7cfc6e84672b09cf9fb923
BETH WACEKE KIMANI <kbwaceke@gmail.com>
2025-06-11 10:33:21 +0300

    Update Header and Footer components: Replaced contact and location details with REM Waste company info based in NR32, UK.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,9 +6,9 @@ const Header = () => {
     <div className="bg-gray-800 text-white p-4 flex flex-col md:flex-row justify-between items-center">
       <div className="flex flex-col md:flex-row items-center space-y-2 md:space-y-0 md:space-x-4">
         <div>9:00 AM - 05:00 PM</div>
-        <div>+1234567890</div>
-        <div><a href="mailto:info@gmail.com">info@gmail.com</a></div>
-        <div>1236 Locust St, Kansas City, MO 65442</div>
+        <div><a href="tel:+441234567890">+44 1234 567890</a></div>
+        <div><a href="mailto:info@remwaste.co.uk">info@remwaste.co.uk</a></div>
+        <div>NR32, UK</div>
       </div>
       <div className="flex space-x-4 mt-2 md:mt-0">
         <a href="#" className="hover:text-gray-300"><FaFacebookF /></a>
